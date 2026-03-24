5d83fb0428713f844813115f81ffb5e86fe686f5
DiegoAndreLeffa <diegodocanto@gmail.com>
2026-03-24 14:00:31 -0300

    feat: implement authentication module with JWT and user registration/login

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ativa a validação global e remove campos indesejados (whitelist)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Habilita CORS (necessário para o Front-end Next.js conseguir bater na API)
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [],
-  exports: [MongooseModule], // Exportamos para que o AuthModule possa usar depois
+  providers: [UsersService],
+  exports: [MongooseModule, UsersService],
 })
 export class UsersModule {}
